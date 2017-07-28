50e5c4a945a30a375123bd69f49d027d55f7b129
derpyfrs <simon_chanct@hotmail.com>
2017-07-28 06:48:40 +0000

    renamed var for clarity

--- wk1d4/mapfunction.js
+++ wk1d4/mapfunction.js
@@ -1,11 +1,11 @@
 var words = ["ground", "control", "to", "major", "tom"];
 
 function map(arr, cb) {
-  var list = [];
+  var newArr = [];
   for (var i = 0; i < arr.length; i++) {
-    list.push(cb(arr[i]));
+    newArr.push(cb(arr[i]));
   }
-  return list;
+  return newArr;
 }
 
 console.log(map(words, function(word){
